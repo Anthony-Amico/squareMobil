d15df272be304a382f78ed01941d9f1bb4929d78
ludo <ludovic.peysson@laposte.net>
2023-05-12 11:45:45 +0200

    BottomNavBar

--- lib/src/BottomNavBar.dart
+++ lib/src/BottomNavBar.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class BottomNavBar extends StatelessWidget {
-  const BottomNavBar({
-    super.key,
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({Key? key}) : super(key: key);
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _currentIndex = 0;
+
+  setCurrentIndex(int index){
+  setState(() {
+  _currentIndex = index;
   });
+  }
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      currentIndex: _currentIndex,
+      onTap: (index) => setCurrentIndex(index),
       selectedItemColor: Colors.red,
       unselectedItemColor: Colors.grey,
       iconSize: 36,
@@ -22,4 +35,5 @@ class BottomNavBar extends StatelessWidget {
     );
   }
 }
+
 
